906ad1dc7b9843856c786afe0c9428a1f1d6b6b0
kalyani <a.lakshmikalyani@gmail.com>
2015-04-06 11:12:19 +0530

    core code initial commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart2/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart2/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart2/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart2/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart2/admin/');
+define('DIR_SYSTEM', '/var/www/opencart2/system/');
+define('DIR_LANGUAGE', '/var/www/opencart2/admin/language/');
+define('DIR_TEMPLATE', '/var/www/opencart2/admin/view/template/');
+define('DIR_CONFIG', '/var/www/opencart2/system/config/');
+define('DIR_IMAGE', '/var/www/opencart2/image/');
+define('DIR_CACHE', '/var/www/opencart2/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencart2/system/download/');
+define('DIR_UPLOAD', '/var/www/opencart2/system/upload/');
+define('DIR_LOGS', '/var/www/opencart2/system/logs/');
+define('DIR_MODIFICATION', '/var/www/opencart2/system/modification/');
+define('DIR_CATALOG', '/var/www/opencart2/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart2');
+define('DB_PREFIX', 'oc_');
